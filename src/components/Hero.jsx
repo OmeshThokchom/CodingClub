8290eed972aa276fea8fb662fbde6d958ea13461
OmeshThokchom <thokchomomesh@gmail.com>
2025-10-17 21:03:37 +0530

    mobile responsiveness#1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,15 +6,11 @@ const hero = () => {
     <main className="flex pt-20 flex-col lg:flex-row items-centre justify-between min-h-[calc(90vh-6rem)]">
         <div data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine" className="max-w-xl ml-[5%] z-10 lg:mt-0">
             <div className='relative w-[95%] sm:w-48 h-10 bg-gradient-to-r from-[#656565] to-[#e99b63] shadow-[0_0_15px_rgba(255,255,255,0.4)] rounded-full'>
-                <div className='absolute inset-[3px] bg-black rounded-full flex items-center justify-center gap-1' >
+                <div className='absolute inset-[3px] bg-black rounded-full flex items-center justify-center' >
                     <i class='bx bx-diamond'></i>CODE HIVE
-
                 </div>
             </div>
 
-            <h1 className='text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold tracking-wider my-8'>
-              CODE HIVE
-            </h1>
             <p className='text-base sm:text-lg tracking-wider text-gray-400 max-w-[25rem] lg:max-w-[30rem]'>
               Our vision is to build a community where every student who wants to code can code, empowered with the right guidance, mentorship, and opportunities. From helping freshers overcome their first coding challenges to supporting seniors in choosing the right competitions and preparing effectively, we aim to make coding accessible, collaborative, and inspiring for all at Royal Global University.
             </p>
